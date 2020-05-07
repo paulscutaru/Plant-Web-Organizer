b131d5c346f0e8d0e56144db627810175cab8820
AlexandruPanainte <61983928+AlexandruPanainte@users.noreply.github.com>
2020-05-07 18:34:46 +0300

    AddAlbums functions

--- core/init.php
+++ core/init.php
@@ -5,7 +5,7 @@ require 'database/connect.php';
 require 'functions/general.php';
 require 'functions/users.php';
 require 'functions/plants.php';
- 
+require 'functions/album.php';
 /*if (logged_in() === true) {
 	$session_user_id = $_SESSION['user_id'];
 	$user_data = user_data($session_user_id, 'user_id', 'username', 'password', 'email', 'date');
