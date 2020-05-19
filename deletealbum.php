c73fbece6f21a386160bdef4141be47ea009b8f6
paulscutaru <61314329+paulscutaru@users.noreply.github.com>
2020-05-19 18:09:16 +0300

    Bug fix delete album

--- deletealbum.php
+++ deletealbum.php
@@ -2,6 +2,8 @@
 /*Script care sterge un album din tabel*/
     include "core/init.php";
     $id = $_GET['id'];
+    $query = "UPDATE plants SET id_album=null WHERE id_album=$id";
+    mysqli_query($con,$query);
     $query = "DELETE FROM albums where id=$id";
     mysqli_query($con,$query);
     header("Location: albums.php");
